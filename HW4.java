a64511e7fe2fa79d9786ecc59eed128d932f79b1
Hao Li <roadmonster1989@gmail.com>
2020-01-31 17:15:29 -0800

    Fixed bugs in passing parameters

--- HW4.java
+++ HW4.java
@@ -16,20 +16,21 @@ import java.util.concurrent.*;
  */
 public class HW4{
     
-    final int NUM_THREADS = 16; 
-    final int ARR_SIZE = 1 <<22; //size of the arr to feed
-    final int TIME_OUT = 10;//in seconds (max wait for an output)
+    static final int NUM_THREADS = 16; 
+    static final int ARR_SIZE = 1 <<22; //size of the arr to feed
+    static final int TIME_OUT = 10;//in seconds (max wait for an output)
     
     public static void main(String[] args){
 
         long start = System.currentTimeMillis();
         int work = 0;
 
+
         while (System.currentTimeMillis() < start + TIME_OUT * 1000) {
 
     
-            int[]arr = getRandomArr(ARR_SIZE);
-            CylicBarrier cb = new CylicBarrier(NUM_THREADS);
+            int[]arr = HW4.getRandomArr(ARR_SIZE);
+            CyclicBarrier cb = new CyclicBarrier(NUM_THREADS);
             Thread[] threads = new Thread[NUM_THREADS];
             for(int i =0; i < NUM_THREADS; i++){
 
@@ -37,7 +38,8 @@ public class HW4{
                 threads[i].start();
             }
 
-            try{
+           
+  				try{
                 for(int j = 0; j < NUM_THREADS; j++){
                     threads[j].join();
                 }
@@ -45,21 +47,26 @@ public class HW4{
                 e.printStackTrace();
                 System.exit(1);
             }
+	
+			
             work++;
-            start = System.currentTimeMillis();
+           	System.out.println(work);
         }
+
+		  System.out.println("Sorted" + work +" arrays (each " + ARR_SIZE + "integers" 
+				+ "in " + TIME_OUT + "seconds");
 
         
     }
     /**
      * Helper class to generate random array
      */
-    public int[]getRandomArr(int size){
-        Randdom rand = new Random();
+    public static int[]getRandomArr(int size){
+        Random rand = new Random();
         int[]arr = new int[size];
         
         for(int i =0; i < size; i++){
-            arr[i] = rand.nextInt();
+            arr[i] = rand.nextInt(101);
         }
         return arr;
     }
