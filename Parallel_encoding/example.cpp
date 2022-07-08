7c0a7fc013362e46630b2d7e10af3f29e4ac4adb
Hao_Li <roadmonster1989@gmail.com>
2022-07-08 16:20:22 -0700

    created example.cpp which shall have 4 threads work their own portion and finally display

---
+++ Parallel_encoding/example.cpp
@@ -0,0 +1,32 @@
+#include "ThreadGroup.h"
+#include <iostream>
+class Example{
+public:
+    void operator()(int id, void*sharedData){
+        std::cout << "Hello from thread " << id << std::endl;
+        std::string *ourData = (std::string*)sharedData;
+        ourData[id] = "done " + std::to_string(id);
+    }
+};
+
+const int N_THREAD = 4;
+
+int main(){
+    std::string ourData[N_THREAD];
+    for(int i = 0; i < N_THREAD; i++){
+        ourData[i] = "";
+    }
+
+    std::cout << "Starting " << N_THREAD << " threads." << std::endl;
+    ThreadGroup<Example>example;
+    for(int i = 0; i < N_THREAD; i++){
+        example.createThread(i, ourData);
+    }
+    example.waitForAll();
+
+    std::cout << "All threads are done! "<< std::endl;
+    for(int i = 0; i < N_THREAD; i++){
+        std::cout << "thread " << i << "wrote "<< ourData[i] << std::endl;
+    }
+    return 0;
+}
