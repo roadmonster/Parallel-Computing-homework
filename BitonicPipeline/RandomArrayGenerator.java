ff543178b9591e92ea1184256902d0028b348781
Hao_Li <roadmonster1989@gmail.com>
2022-07-28 15:25:45 -0700

    RandomArray done

---
+++ BitonicPipeline/RandomArrayGenerator.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class RandomArrayGenerator implements Runnable{
+    private static final int timeout = 10;
+    private int size;
+    private SynchronousQueue<double[]>output;
+    public RandomArrayGenerator(int size, SynchronousQueue<double[]>output) {
+        this.size = size;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                output.offer(getArray(this.size), timeout * 1000, TimeUnit.MILLISECONDS)
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        
+    }
+
+    public static double[] getArray(int len){
+        double[] ret = new double[len];
+        for(int i = 0; i < len; i++){
+            ret[i] = ThreadLocalRandom.current().nextDouble() * 100.0;
+        }
+        return ret;
+    }
+
+    public static boolean isSorted(double[] a){
+        if (a == null)
+            return false;
+        double last = a[0];
+        for(int i = 1; i < a.length; i++){
+            if (a[i] < last)
+                return false;
+            last = a[i];
+        }
+        return true;
+    }
+
+
+}
