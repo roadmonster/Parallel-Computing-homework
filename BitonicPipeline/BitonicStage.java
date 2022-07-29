278759af57b48ac95108ddc8bd99cdc22b6b369a
Hao_Li <roadmonster1989@gmail.com>
2022-07-29 15:27:17 -0700

    found one problem in input1 queue, needs attention

--- BitonicPipeline/BitonicStage.java
+++ BitonicPipeline/BitonicStage.java
@@ -1,8 +1,13 @@
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
+// /**
+//  * Class doing the Bitonic sort
+//  * Takes 2 input queue and 1 output queue
+//  */
 public class BitonicStage implements Runnable {
     private static final int timeout = 10;
+        public enum UpDown {UP, DOWN}
 
     private SynchronousQueue<double[]>input1;
     private SynchronousQueue<double[]>input2;
@@ -20,44 +25,68 @@ public class BitonicStage implements Runnable {
         this.name = name;
     }
 
-    public enum Direction {UP, DOWN}
-
+    public BitonicStage() {
+    }
     @Override
     public void run() {
         double[] array1 = new double[1], array2 = new double[1];
         while(this.output != null && array1 != null && array2 != null){
+                // try {
+                //     // take the input array
+                //     array1 = input1.poll(timeout * 1000, TimeUnit.MILLISECONDS);
+                //     array2 = input2.poll(timeout * 1000, TimeUnit.MILLISECONDS);
+                //     // if input array not null, safe to process and fill the result into the output array
+                //     if (array1 != null && array2 != null){
+                //         double[] a = process(array1, array2);
+                //         output.offer(a, timeout*1000, TimeUnit.MILLISECONDS);
+                //     }else{ // input array is null, display error message
+                //         System.out.println(getClass().getName() + " " + name + " got null array");
+                //     }
+                // } catch (InterruptedException e) { // in case of interruption, return
+                //     return;
+                // }
                 try {
                     array1 = input1.poll(timeout * 1000, TimeUnit.MILLISECONDS);
-                    array2 = input2.poll(timeout * 1000, TimeUnit.MILLISECONDS);
-
-                    if (array1 != null && array2 != null){
-                        double[] a = process(array1, array2);
-                        output.offer(a, timeout*1000, TimeUnit.MILLISECONDS);
-                    }else{
-                        System.out.println(getClass().getName() + " " + name + " got null array");
+                    array2 = input1.poll(timeout * 1000, TimeUnit.MILLISECONDS);
+                    if (array1 != null && array2 != null) {
+                        double a[] = process(array1, array2);
+                        output.offer(a, timeout * 1000, TimeUnit.MILLISECONDS);
+                    } else {
+                        System.out.println(getClass().getName() + " " + name + " got null array as input.");
                     }
                 } catch (InterruptedException e) {
                     return;
                 }
         }
     }
+
+    /**
+     * Entry point / Wrapper method for bitonic sort
+     * This method resue data array if size is right, or re-create one if size of not correct. 
+     * Doing this could save lots of memory since we are doing a stream of arrays, we want to avoid overhead
+     * We fill the data array first half ascending order, then 2nd half decending order
+     * Thenwe  call the sorting method, we start index, length of current array, and the target direction
+     * @param half1
+     * @param half2
+     * @return
+     */
     public double[] process(double[] half1, double[] half2){
         if (this.data == null || this.data.length != 2 * half1.length){
-            this.data = new double[half1.length * 2];
+            this.data = new double[half1.length + half2.length];
         }
         int i = 0; 
         for (int j = 0; j < half1.length; j++){
             data[i++] = half1[j];
         }
 
-        for(int j = half1.length - 1; j >= 0; j--){
+        for(int j = half2.length - 1; j >= 0; j--){
             data[i++] = half2[j];
         }
-        bitonic_sort(0, data.length, Direction.UP);
+        bitonic_sort(0, data.length, UpDown.UP);
         return data;
     }
 
-    private void bitonic_sort(int start, int len, Direction d){
+    private void bitonic_sort(int start, int len, UpDown d){
         if (len > 1){
             int half = len / 2;
             bitonic_merge(start, len, d);
@@ -74,10 +103,10 @@ public class BitonicStage implements Runnable {
         }
     }
 
-    private void bitonic_merge(int start, int len, Direction d){
+    private void bitonic_merge(int start, int len, UpDown d){
         if (len > 1){
             int half = len / 2;
-            if (d == Direction.UP){
+            if (d == UpDown.UP){
                 for (int i = start; i < start + half; i++){
                     if(data[i] > data[i + half])
                         swap(i, i + half);
@@ -90,5 +119,7 @@ public class BitonicStage implements Runnable {
             }
         }
     }
-    
 }
+
+
+
