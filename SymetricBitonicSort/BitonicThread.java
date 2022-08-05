f3843a8869f59b4a2092f23321b67c72600b12c7
Hao_Li <roadmonster1989@gmail.com>
2022-08-05 15:22:19 -0700

    added comments and finish the utility method, awaitBarrier method

--- SymetricBitonicSort/BitonicThread.java
+++ SymetricBitonicSort/BitonicThread.java
@@ -31,26 +31,105 @@ public class BitonicThread implements Runnable{
     }
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        for(int k = 2; k <= array.length; k *= 2){
+            for(int j = k / 2; j > 0; j /= 2){
+                awaitBarrier(j);
+                // once every node within this barrier reached
+                for( int i = start; i < end; i++){
+                    int ixj = i ^ j;
+                    // only care about those greater than current index
+                    if (i < ixj){
+                        // k is alway even, hence & operation will get 0 or 1111...
+                        // hence if result is 0, we regard this as going up
+                        if ((i & k) == 0)
+                            compareAndSwapUp(i, ixj);
+                        else   
+                            compareAndSwapDown(i, ixj);
+                    }
+                }
+            }
+        }
+    }
+
+    private void compareAndSwapDown(int a, int b){
+        if(array[a] < array[b]){
+            double temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+    private void compareAndSwapUp(int a, int b){
+        if(array[a] > array[b]){
+            double temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+
+
+    /**
+     * Figure out which barrier to use and wait for it
+     * @param j 
+     */
+    public void awaitBarrier(int j){
+        int jsweep = j * 2;
+        int size = end - start;
+        int n = array.length;
+        if(jsweep < size)
+            return;
         
+        // start i's value from 1, i works the first node of each level
+        // because the very fist barrier start at index 1 in the barrier array
+        int width = n/2, i = 1;
+        // loop until either width is less or equal current span or i is greater or equal number of levels
+        // this is to find the immediate next width that covers current jweep size
+        while(width > jsweep && i < barrierHeap.length){
+            width /= 2;
+            i = i * 2 + 1;
+        }
+        // then go back a level which is the last lv
+        width *= 2;
+        i /= 2;
+
+        // decide which barrier to use by compute the offset and 
+        int offset = start / width;
+        // find the node
+        int node = i + offset;
+
+        try {
+            barrierHeap[node].await();
+        } catch (Exception e) {
+            System.out.println( threadIdx + " broken due to: " + e);
+        }
+
+
     }
 
     public static void process(double[]data, int p, int g) throws InterruptedException{
         // structure to hold cyclicbarrier
-        CyclicBarrier[] barriers = new CyclicBarrier[p];
+        // number of cyclic barriers is equal to the level of this heap, 
+        // each level share have two times barriers than prev lv
+        // in which case we need to choose barrier to use
+        CyclicBarrier[] barriers = new CyclicBarrier[(1 << g) - 1];
         
+        // We start with 
         int threadPerBarrier = p;
+        // we use i to hold the number of first node for each level
         int i = 1;
-        
+        // start from node 0 and start creating barriers from lv 1, since on lv 0, each thread shall have no overlap
         for(int node = 0; node < barriers.length; node++){
+            // when we reach the start of the next level, we make i the head of next level
+            // we divide the thread by 2, because on each level of sweep, we mutiply the sweep by 2
+            // hence the number of threads are actually merged
             if(node == i){
-                
                 i = i * 2 + 1;
                 threadPerBarrier /= 2;
             }
+            // The first barrier start at index 1
+            // we put the cyclic barrier with current thread per barrier number of latches
             barriers[node] = new CyclicBarrier(threadPerBarrier);
         }
-
+        // create a list of threads and initialze each thread with BitonicThread with barrier as parameter
         List<Thread> threads = new ArrayList<>();
         for (int t = 0; t < p; t++){
             Thread thread = new Thread(new BitonicThread(data, t, p, barriers));
@@ -76,10 +155,11 @@ public class BitonicThread implements Runnable{
     private double[]array;
     private CyclicBarrier[]barrierHeap;
     private static Random rand = new Random();
+
     private static double[] randomArray(int n){
         double ret[] = new double[n];
         for(int i = 0; i < n; i++){
-            ret[i] = rand.nextDouble() * 100. 0;
+            ret[i] = rand.nextDouble() * 100.0;
         }
         return ret;
     }
